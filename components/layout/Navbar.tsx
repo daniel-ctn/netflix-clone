e712fe3d73671027b4e5ce7848e7c7cd44c3863f
Nguyen Cao Thang <thangnc2@dgroup.co>
2022-04-21 18:25:07 +0700

    build header

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,9 +1,41 @@
 import { FC } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+
+import accountImg from 'public/images/account.png'
 
 interface NavbarProps {}
 
 const Navbar: FC = ({}: NavbarProps) => {
-  return <div>navigation bar</div>
+  return (
+    <header>
+      <div className="flex items-center justify-between space-x-2 md:space-x-10 px-8">
+        <Image
+          src="https://cdn.iphoneincanada.ca/wp-content/uploads/2021/11/Netflix.jpeg"
+          width={200}
+          height={100}
+          alt="Netflix logo"
+        />
+        <ul className="items-center justify-between hidden md:flex space-x-4">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div>
+        <SearchIcon className="hidden sm:inline w-6 h-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="w-6 h-6"/>
+          <Link href="/account">
+              <Image src={accountImg} alt="account" className="rounded" width={32} height={32}/>
+          </Link>
+      </div>
+    </header>
+  )
 }
 
 export default Navbar
---
+++ pages/login.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Login: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Netflix - Login</title>
+                <meta
+                    name='description'
+                    content='Netflix clone created by Daniel'
+                />
+            </Head>
+
+            <main className='h-screen'>
+                Login
+            </main>
+        </>
+    )
+}
+
+export default Login
